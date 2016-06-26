c3fd4d6e589a10b2e077b7ac4e8bc3b8cc3a73d6
andreasantarl <andrea.santarl@gmail.com>
2016-06-26 19:57:11 -0400

    Adds destroy to workout routes
    Adds destroy to workout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :profiles, only: [:show, :create, :update]
-  resources :workouts, only: [:index, :show, :create, :update]
+  resources :workouts, only: [:index, :show, :create, :update, :destroy]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
